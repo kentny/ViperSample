0bad8bb66418ff2f403a259658c25f9865010616
kentny <kentaro.terasaki@gmail.com>
2020-08-31 17:23:10 +0900

    空のプロジェクトを作成

---
+++ ViperSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperSample
+//
+//  Created by Kentaro Terasaki on 2020/08/31.
+//  Copyright © 2020 Kentaro Terasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
